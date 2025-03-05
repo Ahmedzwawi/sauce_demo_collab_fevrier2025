9b6330836970b360f848c51745992ab18874bcb0
ahmed <ahmedzwawi46@gmail.com>
2025-03-05 13:17:46 +0100

    commit1

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 const grep = require("@cypress/grep/src/plugin");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter', // a ajouter
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/my-test-output-[hash].xml',},
   e2e: {
     setupNodeEvents(on, config) {
       require('@cypress/grep/src/plugin')(config);
