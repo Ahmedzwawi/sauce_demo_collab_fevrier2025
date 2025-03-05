337d20df04448b3cb5ec59e78bb6c49c2d493ee9
ahmed <ahmedzwawi46@gmail.com>
2025-03-05 14:20:54 +0100

    ajout execution avec junit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 const grep = require("@cypress/grep/src/plugin");
 module.exports = defineConfig({
-  // reporter: 'cypress-mochawesome-reporter', // a ajouter
+  reporter: 'cypress-mochawesome-reporter',
   reporter: 'junit',
   reporterOptions: {
     mochaFile: 'results/my-test-output-[hash].xml',},
@@ -9,7 +9,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('@cypress/grep/src/plugin')(config);
       grep(on, config);
-      // require('cypress-mochawesome-reporter/plugin')(on);// a ajouter
+      require('cypress-mochawesome-reporter/plugin')(on);
       
       return config;    },
   },
