de829dd77242228e8696e0b7c36cb45287b9205b
ahmed <ahmedzwawi46@gmail.com>
2025-03-05 11:43:44 +0100

    commit1

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+const grep = require("@cypress/grep/src/plugin");
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', // a ajouter
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config);
+      grep(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);// a ajouter
+
+      return config;    },
+  },
+});
